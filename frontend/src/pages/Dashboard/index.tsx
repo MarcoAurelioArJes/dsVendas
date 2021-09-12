b67a7096cb94817ec9acc504fe963faaea076bee
MarcoAurelioArJs <marcoaureliodea.jesus@gmail.com>
2021-09-12 16:31:42 -0300

    Home, navigation

--- frontend/src/pages/Dashboard/index.tsx
+++ frontend/src/pages/Dashboard/index.tsx
@@ -3,6 +3,7 @@ import Footer from "components/Footer/footer";
 import DataTable from "components/DataTable/datatable";
 import BarChart from "components/BarChart/barchart";
 import DonutChart from "components/DonutChart/donutchart";
+import { Link } from "react-router-dom";
 
 function Dashboard() {
     return (
@@ -10,8 +11,12 @@ function Dashboard() {
             <>
                 <NavBar />
                 <div className="container">
-                    <h1 className="text-primary py-3">Dashboard de Vendas</h1>
-
+                    <div className="d-flex justify-content-between align-items-center">
+                        <h1 className="text-primary py-3">Dashboard de Vendas</h1>
+                        <Link to="/" className="btn btn-primary btn-lg">
+                            Voltar ao Ínicio
+                        </Link>
+                    </div>
                     <div className="row px-3">
                         <div className="col-sm-6">
                             <h5 className="text-secondary">Taxa de sucesso (%)</h5>
