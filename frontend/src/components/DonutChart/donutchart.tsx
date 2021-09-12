8d6fe9cadd5a2835a7c6decf5df34f5d9f8cc869
MarcoAurelioArJs <marcoaureliodea.jesus@gmail.com>
2021-09-12 19:23:32 -0300

    DonutChart integration

--- frontend/src/components/DonutChart/donutchart.tsx
+++ frontend/src/components/DonutChart/donutchart.tsx
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import { BASE_URL } from 'components/utils/requests';
+import { useEffect, useState } from 'react';
 import Chart from 'react-apexcharts';
 import { SaleSum } from 'types/sale';
 
@@ -10,21 +11,21 @@ type ChartData = {
 
 function DonutChart() {
 
-    // FORMA ERRADA DE DECLARA VARIAVEIS
-    let chartData : ChartData = { labels: [], series: []}
+    const [chartData, setChartData] = useState<ChartData>({ labels: [], series: []});
 
-    // FORMA ERRADA DE CHAMAR A API
-    axios.get(`${BASE_URL}/sales/sum-by-seller`).then(
-        response => {
-            //data é o corpo da resposta
-            const data = response.data as SaleSum[];
-            const myLabels = data.map(x => x.sellerName);
-            const mySeries = data.map(x => x.sum);
+    useEffect(() => {
+        axios.get(`${BASE_URL}/sales/sum-by-seller`).then(
+            response => {
+                //data é o corpo da resposta
+                const data = response.data as SaleSum[];
+                const myLabels = data.map(x => x.sellerName);
+                const mySeries = data.map(x => x.sum);
+    
+                setChartData({labels: myLabels, series: mySeries});
+            }
+        );
+    }, []);
 
-            chartData = {labels: myLabels, series: mySeries};
-            console.log(chartData);
-        }
-    );
 /*     const mockData = {
         series: [477138, 499928, 444867, 220426, 473088],
         labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
